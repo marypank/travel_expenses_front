e13e94aee027540f989244bf3e4c8f60e23b9834
Maria Pankova <qitelopdo@gmail.com>
2026-07-28 20:17:27 +0300

    add pinia and datetime picker

--- src/main.js
+++ src/main.js
@@ -5,7 +5,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import router from './router';
+import { createPinia } from 'pinia'
+import { VueDatePicker } from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+
+const pinia = createPinia();
 
 createApp(App)
     .use(router)
+    .use(pinia)
+    .component('VueDatePicker', VueDatePicker)
     .mount('#app')
